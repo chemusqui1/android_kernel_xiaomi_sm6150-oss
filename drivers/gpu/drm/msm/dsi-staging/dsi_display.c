70314be8f7cd451a894baabfbfa0b01306ff1fdd
Rasenkai <rasenkai99@gmail.com>
2021-03-02 22:42:09 +0530

    Revert drm notifier commits
    * until we figure out why NVT hates this
    
    This reverts following commits
    
    drm: msm: dsi-staging: cleanup notifier implementation
    drm: msm: dsi-staging: trigger drm notifier
    drm: msm: atomic: do not trigger drm notifier
    
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>
    Change-Id: Id4480a495b48fd1fc097f2595538257f2cf46c03

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -1099,6 +1099,7 @@ int dsi_display_set_power(struct drm_connector *connector,
 {
 	struct dsi_display *display = disp;
 	struct msm_drm_notifier notify_data;
+	int event = power_mode;
 	int rc = 0;
 
 	if (!display || !display->panel) {
@@ -1106,8 +1107,7 @@ int dsi_display_set_power(struct drm_connector *connector,
 		return -EINVAL;
 	}
 
-	notify_data.data = &power_mode;
-	notify_data.id = MSM_DRM_PRIMARY_DISPLAY;
+	notify_data.data = &event;
 
 	switch (power_mode) {
 	case SDE_MODE_DPMS_LP1:
